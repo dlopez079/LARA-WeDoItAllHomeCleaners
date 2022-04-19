26e8fea32cdfb77a306832dcc8bc4dc4eb9a5818
David Lopez <dlopez@mattmen.com>
2022-04-19 10:17:48 -0400

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>We Do All Home Cleaners</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}"  rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+        <section>Hero Section</section>
+        <section>Gallery Section</section>
+        <section>Testimonial Section</section>
+        <section>About / Services</section>
+        <section>Contact Us</section>
+    </body>
+</html>
